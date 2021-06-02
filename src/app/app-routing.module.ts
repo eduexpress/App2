74762fa74cd3569dbcdedd41bfa73f2c0f2508d1
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2021-06-02 11:12:54 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,22 @@ import {OrderPageComponent} from "./components/order-page/order-page.component";
 import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
 import {OrderGuard} from "./guards/order.guard";
 import {LoginPageComponent} from "./components/login-page/login-page.component";
+import {SaveCustomerPageComponent} from "./components/customer-page/items/save-customer-page/save-customer-page.component";
+import {DeleteCustomerPageComponent} from "./components/customer-page/items/delete-customer-page/delete-customer-page.component";
+import {UpdateCustomerPageComponent} from "./components/customer-page/items/update-customer-page/update-customer-page.component";
+import {SearchCustomerPageComponent} from "./components/customer-page/items/search-customer-page/search-customer-page.component";
 
 const routes: Routes = [
   /*{path:'', component: CustomerPageComponent},*/
   /*{path: '**', component:NotFoundPageComponent}*/
   {path:'', redirectTo:'/customer', pathMatch:'full'},
-  {path:'customer', component:CustomerPageComponent},
+  {path:'customer', component:CustomerPageComponent, children: [
+      {path: '', redirectTo: '/customer/save-customer', pathMatch:'full'},
+      {path: 'save-customer', component: SaveCustomerPageComponent},
+      {path: 'delete-customer', component: DeleteCustomerPageComponent},
+      {path: 'update-customer', component: UpdateCustomerPageComponent},
+      {path: 'search-customer', component: SearchCustomerPageComponent}
+    ]},
   {path:'item', component:ItemPageComponent},
   {path:'order', component:OrderPageComponent, canActivate:[OrderGuard]},
   {path:'login', component:LoginPageComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { OrderPageComponent } from './components/order-page/order-page.component
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import {RouterModule} from "@angular/router";
+import { SaveCustomerPageComponent } from './components/customer-page/items/save-customer-page/save-customer-page.component';
+import { DeleteCustomerPageComponent } from './components/customer-page/items/delete-customer-page/delete-customer-page.component';
+import { UpdateCustomerPageComponent } from './components/customer-page/items/update-customer-page/update-customer-page.component';
+import { SearchCustomerPageComponent } from './components/customer-page/items/search-customer-page/search-customer-page.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import {RouterModule} from "@angular/router";
     ItemPageComponent,
     OrderPageComponent,
     NotFoundPageComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    SaveCustomerPageComponent,
+    DeleteCustomerPageComponent,
+    UpdateCustomerPageComponent,
+    SearchCustomerPageComponent
   ],
   imports: [
     BrowserModule,
