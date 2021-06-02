ffc24b6fe0641da23de0b77a44b6ab2a0dd18ee4
hasika-sandaruwan <hasikasandaruwan.info@gmail.com>
2021-06-02 10:35:32 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CustomerPageComponent} from "./components/customer-page/customer-page.component";
+import {ItemPageComponent} from "./components/item-page/item-page.component";
+import {OrderPageComponent} from "./components/order-page/order-page.component";
+import {NotFoundPageComponent} from "./components/not-found-page/not-found-page.component";
+import {OrderGuard} from "./guards/order.guard";
+import {LoginPageComponent} from "./components/login-page/login-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*{path:'', component: CustomerPageComponent},*/
+  /*{path: '**', component:NotFoundPageComponent}*/
+  {path:'', redirectTo:'/customer', pathMatch:'full'},
+  {path:'customer', component:CustomerPageComponent},
+  {path:'item', component:ItemPageComponent},
+  {path:'order', component:OrderPageComponent, canActivate:[OrderGuard]},
+  {path:'login', component:LoginPageComponent},
+  {path: '**', component:NotFoundPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerPageComponent } from './components/customer-page/customer-page.component';
+import { ItemPageComponent } from './components/item-page/item-page.component';
+import { OrderPageComponent } from './components/order-page/order-page.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerPageComponent,
+    ItemPageComponent,
+    OrderPageComponent,
+    NotFoundPageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
